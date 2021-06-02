3720425e2cea3eb8c48bc1b6e6ad9f39f75f6ddb
Kiran-Ahir <kiranj695@gmail.com>
2021-06-02 13:18:05 +0530

    Visual Polish Done

--- app/src/main/java/com/example/Miwok/Word.java
+++ app/src/main/java/com/example/Miwok/Word.java
@@ -1,10 +1,22 @@
 package com.example.Miwok;
 
+import com.example.miwok.R;
+
 public class Word {
 
     private String mDefaultTranslations;
 
     private String mMiwokTranslations;
+
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+    public Word(String mDefaultTranslations, String mMiwokTranslations, int mImageResourceId) {
+        this.mDefaultTranslations = mDefaultTranslations;
+        this.mMiwokTranslations = mMiwokTranslations;
+        this.mImageResourceId = mImageResourceId;
+    }
 
     public Word(String DefaultTranslations, String MiwokTranslations) {
         mDefaultTranslations = DefaultTranslations;
@@ -18,4 +30,13 @@ public class Word {
     public String getmMiwokTranslations() {
         return mMiwokTranslations;
     }
+
+    public int getmImageResourceId() {
+        return mImageResourceId;
+    }
+
+    public boolean hasImage(){
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
 }
--- app/src/main/java/com/example/Miwok/WordAdapter.java
+++ app/src/main/java/com/example/Miwok/WordAdapter.java
@@ -1,12 +1,15 @@
 package com.example.Miwok;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.miwok.R;
@@ -15,12 +18,15 @@ import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    public WordAdapter(Activity context, ArrayList<Word> words) {
+    private int mColorResourceId;
+
+    public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId) {
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
         // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
         // going to use this second argument, so it can be any value. Here, we used 0.
         super(context, 0, words);
+        mColorResourceId = colorResourceId;
     }
 
     @NonNull
@@ -46,6 +52,23 @@ public class WordAdapter extends ArrayAdapter<Word> {
         // Get the version number from the current AndroidFlavor object and
         // set this text on the number TextView
         defaultTextView.setText(currentWord.getmDefaultTranslations());
+
+        //Image
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+
+        if(currentWord.hasImage()){
+            imageView.setImageResource(currentWord.getmImageResourceId());
+
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            imageView.setVisibility(View.GONE);
+        }
+
+        View textContainer = listItemView.findViewById(R.id.text_container);
+
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+
+        textContainer.setBackgroundColor(color);
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
